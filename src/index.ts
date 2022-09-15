2180cf6f95482334432c236c5d6059dec6754f35
Ilya Sinegubkin <ilya.sinegubkin@gmail.com>
2022-09-15 11:50:34 +0300

    feat: simple backend for todo app

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,11 @@ import { data } from "./mocks";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(bodyParser.json());
 
 const port = 4000;
@@ -14,13 +18,16 @@ const port = 4000;
 let todos: any[] = data;
 
 app.post("/update", (req, res) => {
-  console.log(req.body);
-  todos = req.body.todos;
+  console.log("updated", req.body);
+  todos = req.body;
 
   return res.status(200).json({ success: true });
 });
 
-app.get("/list", (req, res) => res.status(200).json({ todos }));
+app.get("/list", (req, res) => {
+  console.log("get list");
+  res.status(200).json({ todos });
+});
 
 app.listen(port, () =>
   console.log(`Backend running on http://localhost:${port}!`)
